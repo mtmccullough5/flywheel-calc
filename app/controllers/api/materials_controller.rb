e5a2585651069a0a204dab9360b98e310ffdfe49
Matthew McCullough <mcculloughmatthew5@gmail.com>
2018-06-21 22:48:06 -0600

    Api endpoints add

--- app/controllers/api/materials_controller.rb
+++ app/controllers/api/materials_controller.rb
@@ -1,13 +1,48 @@
 class Api::MaterialsController < ApplicationController
-  def create
+  before_action :set_app, only: [:get_properties, :update, :destroy]
+  
+  def index    
+    render json: Material.all  
+  end 
+
+  def get_properties
+    render json: @material  
+  end 
+
+  def create    
+    material = Material.new(material_params)    
+    if material.save      
+      render json: material    
+    else      
+      render json: { errors: material.errors.full_messages.join(', ') }    
+    end  
   end
 
-  def update
+  def update    
+    if @material.update(material_params)      
+      render json: @material    
+    else      
+      render json: { errors: material.errors.full_messages.join(', ') }    
+    end  
   end
 
-  def destroy
+  def destroy    
+    @material.destroy  
   end
 
-  def prop_request
+  private
+
+  def set_material    
+    @material = Material.find(params[:id])  
   end
+
+  def material_params    
+    params.require(:material).permit(      
+      :name,      
+      :density,
+      :yield_strength,      
+      :poisson_ratio,   
+      )  
+    end 
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,1 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Material.create(name: 'steel', density: 3.14, yield_strength: 3.14, poisson_ratio: 4.5)
